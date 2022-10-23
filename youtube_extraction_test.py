ea94fdbee9832e01adfe25b89b7b7e89d02c0acf
bernardolima95 <bernardolima95@gmail.com>
2022-10-23 20:20:28 -0300

    New documentation and added ID fields to comments and authors.

--- youtube_extraction_test.py
+++ youtube_extraction_test.py
@@ -4,6 +4,6 @@ from youtube_extractor import youtube_extractor
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-video_list = ["https://www.youtube.com/watch?v=XnlT3rPNUp0"]
+video_list = ["https://www.youtube.com/watch?v=PXKy_DNQ7Ts", "https://www.youtube.com/watch?v=ZQW10xzM8xI"]
 
-youtube_extractor.extract_video_list(video_list, get_replies = False, to_json = True, path = ".")
+youtube_extractor.extract_video_list(video_list, get_replies = True, to_json = True, path = ".")
